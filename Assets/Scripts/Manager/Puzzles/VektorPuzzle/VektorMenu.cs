82193d19093b3130e206840412d080ef606d7fbb
Westorly <147486500+Westorly@users.noreply.github.com>
2025-05-08 13:33:18 +0200

    Update VektorMenu.cs

--- Assets/Scripts/Manager/Puzzles/VektorPuzzle/VektorMenu.cs
+++ Assets/Scripts/Manager/Puzzles/VektorPuzzle/VektorMenu.cs
@@ -2,10 +2,7 @@ using UnityEngine;
 
 public class VektorMenu : MonoBehaviour
 {
-    public InputField line1InputField; // Assign in inspector
 
-    public InputField line2InputField; // Assign in inspector
-    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
